01bfc542f6d035580878bf88999f50da4ac987db
Farrel Wiratama <50479805+Frllzwrtmn@users.noreply.github.com>
2024-03-21 21:49:09 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-97d88eb3-0d1b-420e-82eb-af77002713bc/)
 
 
 echo "$curl_output" >> Update.tsx
